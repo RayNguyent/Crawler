72dfbdf220e3d63cfb2037a61cecdd69a8b2e7df
RayNguyent <hoang2152002@gmail.com>
2023-06-25 00:03:59 -0700

    updated my_spider.py, still need to fix duplicate

--- crawler/spiders/my_spider.py
+++ crawler/spiders/my_spider.py
@@ -1,13 +1,23 @@
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
+import scrapy
 
-class CrawlingSpider(CrawlSpider):
+class CrawlingSpider(scrapy.Spider):
     name = 'mycrawler'
-    allow_domains = ['.com.vn']
-    start_urls = ['https://batdongsan24h.com.vn']
+    start_urls = ['https://nhadat24h.net/nha-dat-ban']
+    
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url,dont_filter=False)
 
-    rules = (
-        Rule(LinkExtractor(allow='bat-dong-san-ban-tai-viet-nam-s32113')),
-        Rule(LinkExtractor(allow='bat-dong-san-cho-thue-tai-viet-nam-s34397')),
-
-    )
+    def parse(self,response):
+        for items in response.css("div.dv-item"):
+            yield {
+                'title': response.css('div.pn1 a::attr(title)').get(),
+                #'time' : response.css('p.time::text').get(), #with respect to current time of browsed => Fix later
+                #'price': response.css('label.a-txt-cl1::text').get(),
+                #'area' :response.css('label.a-txt-cl2::text').get(),
+                #'neighborhood': response.css('label.rvVitri ::text').get(),
+                #'description' : response.css('label.lb-des ::text').get()
+            }
+        next_page = response.css('div.dv-pt-item  a::attr(href)').getall()[1]
+        if next_page is not None:
+            yield response.follow(next_page,callback = self.parse)
