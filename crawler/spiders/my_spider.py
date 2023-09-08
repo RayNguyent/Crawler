5ad560db24da4129fe9bf71609544ab1db00c7c2
RayNguyent <hoang2152002@gmail.com>
2023-09-08 08:20:34 -0700

    newly crawled 090723

--- crawler/spiders/my_spider.py
+++ crawler/spiders/my_spider.py
@@ -2,7 +2,7 @@ import scrapy
 
 class CrawlingSpider(scrapy.Spider):
     name = 'mycrawler'
-    start_urls = ['https://nhadat24h.net/nha-dat-ban',
+    start_urls = [#'https://nhadat24h.net/nha-dat-ban']
                 'https://nhadat24h.net/nha-dat-cho-thue']
     
     def start_requests(self):
@@ -25,24 +25,28 @@ class CrawlingSpider(scrapy.Spider):
         for img_url in raw_images_urls:
             clean_img_urls.append(response.urljoin(img_url))
         yield {
-            'title': response.css('#txtcontenttieudetin::text').get(),
-            'type': response.css('#ContentPlaceHolder1_ctl00_lbLoaiBDS::text').get(),
-            'time' : response.css('p.time::text').get(), 
-            'price': ' '.join([response.css('.strong1::text').get(),response.css("label.lb-pri-dt").get().split('</label')[1].split('>')[1].split(' ')[0]]),
-            'area' : ' '.join([response.css('.strong2::text').get(),response.css("label.lb-pri-dt").get().split('</label')[2].split('>')[1]]),
-            'land_title_certificate': response.css(".dv-time-dt strong::text").get(),
-            'location': response.css("#ContentPlaceHolder1_ctl00_lbTinhThanh::text").get(),
-            'description' : '  '.join(response.css(".dv-txt-mt ::text").getall()),
-            'beds': response.css('.dv-time-dt+ .dv-tsbds tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
-            'floors': response.css('.dv-tsbds:nth-child(5) tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
-            'road_width': response.css('.dv-tsbds:nth-child(6) tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
-            'parking_slots': response.css('.dv-tsbds:nth-child(7) tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
-            'washrooms': response.css('.dv-time-dt+ .dv-tsbds tr+ tr .col1+ td').get().split('<td>')[1].split('</td>')[0],
-            'direction': response.css('#ContentPlaceHolder1_ctl00_lbHuong::text').get(),
-            'land_width': response.css('.dv-tsbds:nth-child(6) tr+ tr .col1+ td').get().split('<td>')[1].split('</td>')[0],
-            'id': response.css('.dv-tsbds:nth-child(7) tr+ tr .col1+ td').get().split('<td>')[1].split('</td>')[0],
-            'seller': response.css('.fullname a::text').get(),
-            'company': response.css('.fullname+ label::text').get(),
-            'contact':response.css('#viewmobinumber::text').get(),
+            'Category': 'sale',
+            'ID': response.css('.dv-tsbds:nth-child(7) tr+ tr .col1+ td').get().split('<td>')[1].split('</td>')[0],
+            'Title': response.css('#txtcontenttieudetin::text').get(),
+            'Price': ' '.join([response.css('.strong1::text').get(),response.css("label.lb-pri-dt").get().split('</label')[1].split('>')[1].split(' ')[0]]),
+            'Area' : ' '.join([response.css('.strong2::text').get(),response.css("label.lb-pri-dt").get().split('</label')[2].split('>')[1]]),
+            'Location': response.css("#ContentPlaceHolder1_ctl00_lbTinhThanh::text").get(),
+            'Estate type': response.css('#ContentPlaceHolder1_ctl00_lbLoaiBDS::text').get(),
+            'Direction': response.css('#ContentPlaceHolder1_ctl00_lbHuong::text').get(),
+            'Rooms': '',
+            'Bedrooms': response.css('.dv-time-dt+ .dv-tsbds tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
+            'Kitchen': '',
+            'Living room': '',
+            'Bathrooms': response.css('.dv-time-dt+ .dv-tsbds tr+ tr .col1+ td').get().split('<td>')[1].split('</td>')[0],
+            'Front_width': response.css('.dv-tsbds:nth-child(6) tr+ tr .col1+ td').get().split('<td>')[1].split('</td>')[0],
+            'Floor': response.css('.dv-tsbds:nth-child(5) tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
+            'Parking Slot': response.css('.dv-tsbds:nth-child(7) tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
+            'Description' : '  '.join(response.css(".dv-txt-mt ::text").getall()),
+            'Seller name': response.css('.fullname a::text').get(),
+            'Seller type': response.css('.fullname+ label::text').get(),
+            'Phone':response.css('#viewmobinumber::text').get(),
             'image_urls': clean_img_urls,
+            'Email': '',
+            'Road width': response.css('.dv-tsbds:nth-child(6) tr:nth-child(1) .col1+ td').get().split('<td>')[1].split('</td>')[0],
+            'Sizes': '',
         }
